2b592b493d796122893fe1d50f212af813a97921
GER13117 <okke.warner@gmail.com>
2021-04-27 21:31:26 +0200

    added TODOs

--- src/main.cpp
+++ src/main.cpp
@@ -5,3 +5,12 @@ int main() {
     game.run();
     return 0;
 }
+
+//TODO: Keypresstiming
+//TODO: View / Camera on Player
+//TODO: Map
+//TODO: Infinite
+//TODO: Procedural
+//TODO: Seedsystem(damit man wieder wohin zurückgehen kann und die map nicht konstant gerendert werden muss + Noise (Biome, Städte, Wege)
+//TODO: NPCs (AI System etc.)
+//TODO: Spielziel: Crafting, Mining, PVP, Exploring
