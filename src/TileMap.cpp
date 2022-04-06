01a2e1b8f6669558113c000a342d8cf18ad0f3c0
GER13117 <okke.warner@gmail.com>
2022-04-06 22:12:39 +0200

    removed unused code

--- src/TileMap.cpp
+++ src/TileMap.cpp
@@ -299,28 +299,6 @@ sf::Color TileMap::tileColor(float noise, float textureVariationNoise, float tem
         }
     }
 }
-/*if (noise < -0.500f) {
-    return {2, 43, 68}; // dark blue: deep water
-} else if (noise < -0.10f) {
-    return {9, 62, 92}; // deep blue: water
-} else if (noise < -0.060f) {
-    return {69, 108, 118}; // blue: shallow water
-} else if (noise < 0.010f) {
-    return {207, 209, 134}; // Beach
-} else if (noise < 0.60f) { // grass
-    if (textureVariationNoise < 0.f)
-        return {71, 117, 20};
-    else if (textureVariationNoise < 0.5f)
-        return {94, 135, 50};
-    else
-        return {105, 138, 70};
-} else if (noise < 0.700f) {
-    return {115, 128, 77}; // light green: veld
-} else if (noise < 0.900f) {
-    return {153, 143, 92}; // brown: tundra
-} else {
-    return {179, 179, 179}; // grey: rocks
-}*/
 
 /**
  * Creates a Tile / Object with different parameters given by the player
@@ -403,22 +381,6 @@ void TileMap::update(sf::Vector2f player_position) {
             ++it;
         }
     }
-    //std::cout << tiles.size() << std::endl;
-    /*
-    for (auto e: tiles) {
-        delete e;
-    }
-    this->tiles.clear();
-
-    for (int x = offset.x - maxTilesX * tileSizeX; x < maxTilesX * tileSizeX + offset.x; x += tileSizeX) {
-        for (int y = offset.y - maxTilesY * tileSizeY; y < maxTilesY * tileSizeY + offset.y; y += tileSizeY) {
-            this->tile = new Tile((float) x, (float) y, (float) tileSizeX, (float) tileSizeY,
-                                  tileColor(this->geologicalSimplex->fractal(octaves, (float) x, (float) y) + offsetZ,
-                                            this->grasSimplex->fractal(octaves, (float) x + 9129834.f,
-                                                                       (float) y + 1208012.f) + offsetZ));
-            this->tiles.push_back(this->tile);
-        }
-    }*/
 }
 
 void TileMap::render(sf::RenderTarget &target) {
